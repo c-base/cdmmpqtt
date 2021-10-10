aec1de23d1d31f7239a880fcd27e2884e108b5ef
Uwe Kamper <me@uwekamper.de>
2021-10-10 22:38:11 +0200

    it is 2021 and we are touching this code again

--- cdmmpqtt.py
+++ cdmmpqtt.py
@@ -175,10 +175,10 @@ if __name__ == '__main__':
     mqtt_client_id = "cdmmpqtt%d" % getnode()
 
     root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
+#    root.setLevel(logging.DEBUG)
 
     ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
+#    ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)
---
+++ runenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+source /home/uk/cdmmpqtt/.venv/bin/activate
+
+/home/uk/cdmmpqtt/cdmmpqtt.py
+
